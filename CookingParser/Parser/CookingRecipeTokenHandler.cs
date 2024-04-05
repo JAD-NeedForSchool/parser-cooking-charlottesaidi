478f6a160a2121183a89e97257a61af562bdc4d0
Aliseth <charlotte.saidi@outlook.fr>
2024-04-05 14:29:58 +0200

    implémente la méthode hasNext

--- CookingParser/Parser/CookingRecipeTokenHandler.cs
+++ CookingParser/Parser/CookingRecipeTokenHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CookingParser.Interpreter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace CookingParser.Parser
 {
-    internal class CookingRecipeTokenHandler
+    abstract class CookingRecipeTokenHandler
     {
+        public string token { get; set; }
+        public CookingRecipeTokenHandler next { get; set; }
+
+        public CookingRecipeTokenHandler(string token, CookingRecipeTokenHandler next)
+        {
+            this.token = token;
+            this.next = next;
+        }
+
+        public CookingRecipeTokenHandler(string token)
+        {
+            this.token = token;
+        }
+
+        public bool HasNext()
+        {
+            return this.next == null;
+        }
+
+        public abstract CookingOrder Handle(Recipe recipe);
     }
 }
