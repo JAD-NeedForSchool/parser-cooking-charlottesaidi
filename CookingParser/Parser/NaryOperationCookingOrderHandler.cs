60a375b409b3470caeac43c751e5f3b9285cd23e
Aliseth <charlotte.saidi@outlook.fr>
2024-04-05 14:23:44 +0200

    ajoute les classes du parser

---
+++ CookingParser/Parser/NaryOperationCookingOrderHandler.cs
@@ -0,0 +1,21 @@
+﻿using CookingParser.Interpreter;
+using CookingParser.Operation.Complex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookingParser.Parser
+{
+    internal class NaryOperationCookingOrderHandler(string token, NaryOperation operation) : CookingRecipeTokenHandler
+    {
+        public string token { get; set; } = token;
+        public NaryOperation operation { get; set; } = operation;
+
+        public override CookingOrder Handle(Recipe recipe)
+        {
+            return new NaryOperationCookingOrder();
+        }
+    }
+}
